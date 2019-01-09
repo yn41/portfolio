a37c3e9607dcee56c5b94e7f3355e93a02a21ff5
yuna lee <lyn6264@gomcorp.com>
2019-01-09 18:31:13 +0900

    스크립트 추가

--- src/js/ui_jquery.js
+++ src/js/ui_jquery.js
@@ -19,3 +19,34 @@ function getInternetExplorerVersion() {
     $("html").addClass("ie"+rv);
 }
 getInternetExplorerVersion();
+
+function fnOpenPop(num) {
+    $(".pop_wrap").fadeIn(100);
+    $(".pop").each(function(i){
+        if($(this).attr("data-idx")== num){
+            $(".pop_wrap").attr("data-trg", $(window).scrollTop());
+            $(this).fadeIn(100,  function(){
+                $("html, body").css({"position":"fixed","overflow":"hidden","width":"100%","height":"100%"}).bind('touchmove');
+            });
+            return;
+        }
+    });
+}
+$(function(){
+    $(".btn_close").click(function(){
+        /*$(".pop_wrap").fadeOut();
+        $(".pop").fadeOut();*/
+        $("html, body").css({"position":"","overflow":"","width":"","height":""}).unbind('touchmove').scrollTop($(".pop_wrap").attr("data-trg"));
+        $(".pop").fadeOut(100, function(){
+            $(".pop_wrap").attr("data-trg", "0")
+            $(".pop_wrap").fadeOut(100);
+        });
+    });
+    $("body").click(function(e){
+        var pop = $(".pop");
+        if(!pop.is(e.target) && pop.has(e.target).length === 0){
+            $(".pop_wrap").hide();
+            $(".pop").hide();
+        }
+    });
+});
