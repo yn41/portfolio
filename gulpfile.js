7be69ad9e7fd182c711d694ef24b7ecfefadad03
Yuna <yn8206@gmail.com>
2019-10-14 09:33:58 +0900

    docs 디렉토리 변경

--- gulpfile.js
+++ gulpfile.js
@@ -10,33 +10,37 @@ var gulp = require('gulp'),
 var paths = {
     port:'3000',
     input:'./src/**/*',
-    output:'./public/',
+	output:'./docs/',
+	build:'./public/',
     html: {
         input: './src/**/*.html',
-        output: './public/'
+        output: './docs/'
     },
 	img:{
 		input:'./src/img/*',
-		output:'./public/img'
+		output:'./docs/img'
 	},
 	font:{
 		input:'./src/font/*',
-		output:'./public/font'
+		output:'./docs/font'
 	},
     style:{
         input:'./src/scss/*.scss',
-        output:'./public/css'
+        output:'./docs/css'
     },
     js: {
         input : './src/js/*.js',
-        output : './public/js/'
+        output : './docs/js/'
     }
 };
 
 //browser-sync
 gulp.task('browserSync', function () {
     console.log("gulp 시작!!");
-
+	gulp.src(paths.font.input)
+	.pipe(gulp.dest(paths.font.output));
+	gulp.src(paths.img.input)
+	.pipe(gulp.dest(paths.img.output));
     return browserSync.init({
         port:paths.port,
         server:{
@@ -51,32 +55,32 @@ gulp.task('html-include', function() {
     return gulp.src([
         paths.html.input
     ])
-        .pipe(html())
-        .pipe(gulp.dest(paths.html.output))
-        .pipe(browserSync.reload({
-            stream:true
-        }))
+	.pipe(html())
+	.pipe(gulp.dest(paths.html.output))
+	.pipe(browserSync.reload({
+		stream:true
+	}))
 });
 
 //scss
 gulp.task('sass', function () {
     gulp.src(paths.style.input)
-        .pipe(sourcemaps.init()) //소스맵 초기화 및 생성
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(sass({outputStyle:'expanded'})) //nested, expanded, compact, compressed
-        .pipe(sourcemaps.write('.')) //소스맵 작성
-        .pipe(gulp.dest(paths.style.output))
-        .pipe(browserSync.reload({
-            stream:true
-        }));
+	.pipe(sourcemaps.init()) //소스맵 초기화 및 생성
+	.pipe(sass.sync().on('error', sass.logError))
+	.pipe(sass({outputStyle:'expanded'})) //nested, expanded, compact, compressed
+	.pipe(sourcemaps.write('.')) //소스맵 작성
+	.pipe(gulp.dest(paths.style.output))
+	.pipe(browserSync.reload({
+		stream:true
+	}));
     //압축버전 - 이버전은 소스맵이 안맞아서 배포용으로 따로 뺌
     gulp.src(paths.style.input)
-        .pipe(sass.sync().on('error', sass.logError))
-        .pipe(autoprefixer({
-            browsers: ['last 2 versions','ie 8', 'ie 9']
-        }))
-        .pipe(sass({outputStyle:'compressed'})) //nested, expanded, compact, compressed
-        .pipe(gulp.dest(paths.style.output+'/min'));
+	.pipe(sass.sync().on('error', sass.logError))
+	.pipe(autoprefixer({
+		browsers: ['last 2 versions','ie 8', 'ie 9']
+	}))
+	.pipe(sass({outputStyle:'compressed'})) //nested, expanded, compact, compressed
+	.pipe(gulp.dest(paths.style.output+'/min'));
 });
 
 gulp.task('js',function(){
@@ -89,17 +93,10 @@ gulp.task('js',function(){
 		}));
 });
 
-gulp.task('copy',function(){
-	gulp.src(paths.font.input)
-		.pipe(gulp.dest(paths.font.output));
-	gulp.src(paths.img.input)
-		.pipe(gulp.dest(paths.img.output));
-});
-
 gulp.task('watch', ['html-include','sass','js'], function() {
     gulp.watch(paths.style.input, ['sass']);
     gulp.watch(paths.html.input, ['html-include']);
 	gulp.watch(paths.js.input, ['js']);
 });
 
-gulp.task('default',['browserSync','watch', "copy"]);
+gulp.task('default',['browserSync','watch']);
